4256cbf1d3fe03f67c05b0dcca0d82d96f0ff624
De Castro Da Silva <BIDEDAJO@intranet.ibermatica>
2019-12-03 16:21:42 +0100

    s

--- iTresnaFront/src/app/p-espacios/p-espacios.component.ts
+++ iTresnaFront/src/app/p-espacios/p-espacios.component.ts
@@ -85,4 +85,6 @@ export class PEspaciosComponent implements OnInit {
   estaEnPermisos(item:CopsItem):boolean{
     return this.usuarioLogado.permisos.filter(x=>x.cod_esp===item.cod_esp && x.cod_cop===item.cod_cop && x.ind_admin).length>0;
   }
+
+
 }
