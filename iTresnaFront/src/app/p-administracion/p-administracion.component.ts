9ad37a7032e921a46213bc04f9f13f02e1ddbf3b
Jessica Rubio <rubiovargas.jessica@gmail.com>
2020-01-03 09:57:20 +0100

    merge

--- iTresnaFront/src/app/p-administracion/p-administracion.component.ts
+++ iTresnaFront/src/app/p-administracion/p-administracion.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { ActivatedRoute, Router } from '@angular/router';
-import { Usuario } from '../clases/usuario';
-=======
 import { UsuariosService} from '../servicios/usuarios.service';
 import { Usuario, Permiso } from '../clases/usuario';
->>>>>>> 77c210cbf376325885e08860908d5f583ae3f8cc
 
 @Component({
   selector: 'app-p-administracion',
@@ -19,15 +14,11 @@ export class PAdministracionComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-<<<<<<< HEAD
-    this.usuarioLogado=JSON.parse(localStorage.getItem("usuario"));
-=======
     if(localStorage.getItem("usuario")!=null){
       this.usuarioLogado=JSON.parse(localStorage.getItem("usuario"));
       console.log(this.usuarioLogado);
     }
 
->>>>>>> 77c210cbf376325885e08860908d5f583ae3f8cc
   }
 
 }
