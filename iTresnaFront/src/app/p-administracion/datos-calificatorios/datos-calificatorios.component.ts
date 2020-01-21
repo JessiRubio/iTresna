7c6ac23c706f69e36ab7d0582dd24c42dff24f5c
Jessica Rubio <rubiovargas.jessica@gmail.com>
2020-01-21 17:16:16 +0100

    rama jess

--- iTresnaFront/src/app/p-administracion/datos-calificatorios/datos-calificatorios.component.ts
+++ iTresnaFront/src/app/p-administracion/datos-calificatorios/datos-calificatorios.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { OrganizacionesService } from '../../servicios/organizaciones.service';
+import { Organizacion } from '../../clases/organizacion';
+import { Usuario } from '../../clases/usuario';
 
 @Component({
   selector: 'app-datos-calificatorios',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatosCalificatoriosComponent implements OnInit {
 
-  constructor() { }
+  private form:FormGroup;
+  private organizacion: Organizacion;
+  private usuarioLogeado: Usuario;
+
+  editando:boolean;
+
+  constructor(private fBuilder: FormBuilder,
+    private organizacionesService: OrganizacionesService) 
+    {
+      this.form=this.fBuilder.group({
+        clasif1:["",Validators.required],
+        clasif2:["",Validators.required],
+        clasif3:["",Validators.required],
+      });
+      this.usuarioLogeado=JSON.parse(localStorage.getItem("usuario"));
+      this.editando = false;
+   }
 
   ngOnInit() {
+    this.cargarOrg();
   }
 
+  cargarOrg(){
+    this.organizacionesService.getOrganizacionActual(this.usuarioLogeado.cod_org).subscribe(
+      response=>{
+        this.organizacion=response.organizacion;
+        this.form=this.fBuilder.group({
+          clasif1:[this.organizacion.clasif1,Validators.required],
+          clasif2:[this.organizacion.clasif2,Validators.required],
+          clasif3:[this.organizacion.clasif3,Validators.required],
+        });
+      }
+    );
+  }
+
+  editar(){
+    this.editando = true;
+  }
+
+  guardarCambios(){
+    this.editando = false;
+  }
 }
