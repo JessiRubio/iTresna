ffd18552ed6d08d6d598ef2bd5ce376d269fd09d
Unai Martin <unaimarg@gmail.com>
2020-01-21 15:50:31 +0100

    cambios back creacion de codigos

--- iTresnaBack/php/organizaciones/post.php
+++ iTresnaBack/php/organizaciones/post.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once("./../conexion.php");
+    include("./../conexion.php");
     $json = file_get_contents('php://input');
     $data = json_decode($json);
     $desc_cop=$data->desc_org;
@@ -10,8 +10,20 @@
         "error"=>1,
         "usuario"=>array()
     );
-    echo $img;
-    if($desc_cop!="" && $eslogan!="" && $result!=""){
+    if($desc_cop!="" && $eslogan!=""){
+        $cod_org=getCodOrg();
+        echo $cod_org;
+    }
 
+    function getCodOrg(){
+        include("./../conexion.php");
+        $sql="SELECT MAX(cod_org)
+            FROM t_org";
+        $query=$conexion->prepare($sql);
+        $query->execute();
+        $query->bind_result($cod_org);
+        $query->fetch();
+        $query->close();
+        return ($cod_org!=null)?($cod_org+1):1;
     }
 ?>
