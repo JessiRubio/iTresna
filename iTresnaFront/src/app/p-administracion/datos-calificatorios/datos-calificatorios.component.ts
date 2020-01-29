946723d7fd74073d482cf23094a5220182d8d49c
Jessica Rubio <rubiovargas.jessica@gmail.com>
2020-01-29 15:13:26 +0100

    Datos calificatorios style

--- iTresnaFront/src/app/p-administracion/datos-calificatorios/datos-calificatorios.component.ts
+++ iTresnaFront/src/app/p-administracion/datos-calificatorios/datos-calificatorios.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { OrganizacionesService } from '../../servicios/organizaciones.service';
-import { Organizacion } from '../../clases/organizacion';
+import { Organizacion, Categorias } from '../../clases/organizacion';
 import { Usuario } from '../../clases/usuario';
 
 @Component({
@@ -9,11 +9,15 @@ import { Usuario } from '../../clases/usuario';
   templateUrl: './datos-calificatorios.component.html',
   styleUrls: ['./datos-calificatorios.component.css']
 })
-export class DatosCalificatoriosComponent implements OnInit {
+export class DatosCalificatoriosComponent implements OnInit{
 
   private form:FormGroup;
   private organizacion: Organizacion;
   private usuarioLogeado: Usuario;
+
+  private listaClasif1:string[];
+  private listaClasif2:string[];
+  private listaClasif3:string[];
 
   editando:boolean;
 
@@ -32,15 +36,17 @@ export class DatosCalificatoriosComponent implements OnInit {
   ngOnInit() {
     this.cargarOrg();
     this.form.disable();
+  
   }
 
   cargarOrg(){
     this.organizacionesService.getOrganizacionActual(this.usuarioLogeado.cod_org).subscribe(
-      response=>{
-        this.organizacion=response.organizacion;
+      res=>{
+        this.organizacion=res.organizacion;
         this.form.controls["clasif1"].setValue(this.organizacion.clasif1,Validators.required);
         this.form.controls["clasif2"].setValue(this.organizacion.clasif2,Validators.required);
-        this.form.controls["clasif3"].setValue(this.organizacion.clasif3,Validators.required);
+        this.form.controls["clasif3"].setValue(this.organizacion.clasif3,Validators.required); 
+
       }
     );
   }
@@ -48,6 +54,7 @@ export class DatosCalificatoriosComponent implements OnInit {
   editar(){
     this.editando = true;
     this.form.enable();
+    this.cargarlistas();
   }
 
   guardarCambios(){
@@ -66,5 +73,22 @@ export class DatosCalificatoriosComponent implements OnInit {
       }
       
     );
+  }
+
+  cargarlistas(){
+    this.listaClasif1 = [];
+    this.listaClasif2 = [];
+    this.listaClasif3 = [];
+    for(var pos=0;pos<this.organizacion.categorias.length;pos++){
+      if (this.organizacion.categorias[pos].tip_clasificacion === this.organizacion.clasif1){
+        this.listaClasif1.push(this.organizacion.categorias[pos].categoria);
+      }else if(this.organizacion.categorias[pos].tip_clasificacion === this.organizacion.clasif2){
+        this.listaClasif2.push(this.organizacion.categorias[pos].categoria);
+      }else if(this.organizacion.categorias[pos].tip_clasificacion === this.organizacion.clasif3){
+        this.listaClasif3.push(this.organizacion.categorias[pos].categoria);
+      }
+    }
+
+
   }
 }
