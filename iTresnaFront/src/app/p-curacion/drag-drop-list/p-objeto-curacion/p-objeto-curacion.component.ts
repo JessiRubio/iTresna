461c9c82f1c8f51def0fcaa6fcc3b35803a6981a
Jessica Rubio <rubiovargas.jessica@gmail.com>
2020-02-28 08:12:50 +0100

    cambio carpeta objeto-curacion

---
+++ iTresnaFront/src/app/p-curacion/drag-drop-list/p-objeto-curacion/p-objeto-curacion.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import { SenalesService } from '../../../servicios/senales.service';
+import { ComentariosService } from '../../../servicios/comentarios.service';
+import { UsuariosService } from '../../../servicios/usuarios.service';
+import { MatDialog } from '@angular/material';
+import { SenalesItem } from '../../../clases/senales-item';
+import { EtiquetaItem } from '../../../clases/copsitem';
+import { Usuario } from '../../../clases/usuario';
+
+@Component({
+  selector: 'app-p-objeto-curacion',
+  templateUrl: './p-objeto-curacion.component.html',
+  styleUrls: ['./p-objeto-curacion.component.css']
+})
+export class PObjetoCuracionComponent implements OnInit {
+
+  private usuarioLogeado:Usuario;
+  private cod_esp:number;
+  private cod_cop:number;
+  private admin:boolean=false;
+  
+  str: string;
+  cod_coment:number=15;
+  mostrarEnvio:boolean=false;
+  
+  @Input() senal:SenalesItem;
+
+  titulo:String= "Lorem Ipsum";
+  imagen:string="./../../../assets/cyberSecurityData.jpg";
+  acortado:boolean=true; 
+
+  constructor(
+    private http:HttpClient,
+    private router: Router,
+    private route: ActivatedRoute,
+    private senalesService:SenalesService,
+    private comentariosService:ComentariosService,
+    private usuarioService:UsuariosService,
+    private dialog:MatDialog
+    ) {
+   }
+
+  ngOnInit() {
+    this.usuarioLogeado=JSON.parse(localStorage.getItem("usuario"));
+    this.route.queryParams.subscribe(params => {
+      this.cod_cop = params['copSeleccionado'];
+      this.cod_esp = params['codEspacio'];
+
+    });
+  }
+
+}
